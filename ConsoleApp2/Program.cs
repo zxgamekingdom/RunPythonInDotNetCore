075416c7ddb7c5d5be6465dcdbe8e9f65f5d3daf
Taurus Zhou <Zhou.Taurus@outlook.com>
2020-03-05 03:31:46 +0800

    添加项目文件。

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using Python.Included;
+using Python.Runtime;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Installer.SetupPython();
+            using (Py.GIL())
+            {
+                string a = "def funcname(a):"
+                           + Environment.NewLine
+                           + "    i = 12"
+                           + Environment.NewLine
+                           + "    a = i+25"
+                           + Environment.NewLine
+                           + "    print(i)"
+                           + Environment.NewLine
+                           + "    print(a)"
+                           + Environment.NewLine
+                           + "    return a";
+                PyScope pyScope = Py.CreateScope();
+                PyObject pyObject = PythonEngine.ModuleFromString("a", a);
+                pyScope.ImportAll(pyObject);
+                PyObject eval = pyScope.Eval("funcname(15)");
+                Console.Out.WriteLine("eval = {0}", eval);
+                eval.GetPythonType().WriteLine();
+                var @as = eval.As<int>();
+                Console.Out.WriteLine("@as = {0}", @as);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public static class ConsoleExtensions
+    {
+        public static void WriteLine(this object o)
+        {
+            Console.WriteLine(o);
+        }
+    }
+}
